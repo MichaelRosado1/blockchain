9519fd6f5b461eba9d52c54f1ffdf55b42d86244
MichaelRosado1 <mrosad4@illinois.edu>
2021-06-19 19:38:38 -0500

    Block chain now works and can process mutliple blocks, but only works in python2 since I am not sure how to fix unicode problem with haslib

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ essentially a public database
 '''
 import hashlib 
 import datetime as date 
+
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
         #each block will have information on the index as well as the 
@@ -20,13 +21,39 @@ class Block:
         #sha256 encryption used in crypto
         shaEncryption = hashlib.sha256()
         #hash the blocks attributes
-        shaEncryption.update(str(self.index) + 
+        shaEncryption.update(str(self.index).encode('utf-8') + 
                              str(self.timestamp) + 
                              str(self.data) + 
                              str(self.previous_hash))
         #return this hash
         return shaEncryption
 
+#first block in the chain
+def createGenesisBlock():
+    #manually construct block
+    return Block(0, date.datetime.now(), 'Genesis Block', '0')
 
-    
+def nextBlock(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = 'I am block ' + str(this_index)
+    this_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_hash)
 
+def main():
+    #intialize list to one block
+    blockchain = [createGenesisBlock()] 
+    previousBlock = blockchain[0]
+
+    #number of blocks we want to create
+    blockChainSize = 10
+
+    for i in range(0, blockChainSize):
+        newBlock = nextBlock(previousBlock)
+        blockchain.append(newBlock)
+        previousBlock = newBlock
+        print "Block #{} has been added".format(newBlock.index)
+        print "Hash: {}\n".format(newBlock.hash) 
+
+if __name__ == '__main__':
+    main()
